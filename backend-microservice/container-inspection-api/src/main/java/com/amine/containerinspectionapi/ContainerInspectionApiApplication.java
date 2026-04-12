969d0c10865a3057e3809d2076b5ec5b8f5d795e
aminegrioui <amine23grioui@gmail.com>
2026-04-12 22:14:55 +0200

    fix: remove nested git from container-inspection-api

---
+++ backend-microservice/container-inspection-api/src/main/java/com/amine/containerinspectionapi/ContainerInspectionApiApplication.java
@@ -0,0 +1,13 @@
+package com.amine.containerinspectionapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContainerInspectionApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContainerInspectionApiApplication.class, args);
+    }
+
+}
---
+++ backend-microservice/container-inspection-api/src/test/java/com/amine/containerinspectionapi/ContainerInspectionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amine.containerinspectionapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContainerInspectionApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
