421aeb1f6e8b181cdc2860679f4be80fbf86cc9a
aminegrioui <amine23grioui@gmail.com>
2026-04-25 22:05:18 +0200

    main.py

--- model_microservice/app/main.py
+++ model_microservice/app/main.py
@@ -6,6 +6,3 @@ app = FastAPI(title="Container AI Service")
 
 
 app.include_router(analyze_router, prefix="/api/v1", tags=["Analysis"])
-
-# if __name__ == "__main__":
-#     uvicorn.run("app.main:app", host="0.0.0.0", port=5000, reload=True)
